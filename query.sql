2e984ef7ee31d977f1aa122fa86fd1e74cc00188
mhaszek <32060715+mhaszek@users.noreply.github.com>
2021-04-11 17:10:10 +1000

    homework final version

--- query.sql
+++ query.sql
@@ -1,25 +1,30 @@
--- 1. List the following details of each employee: employee number, last name, first name, sex, and salary
-SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
-FROM employees e
-JOIN salaries s
-ON e.emp_no = s.emp_no;
+-- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT e.emp_no AS "employee number", e.last_name AS "last name", e.first_name AS "first name", e.sex, s.salary
+FROM employees AS e
+JOIN salaries AS s
+ON e.emp_no = s.emp_no
+ORDER BY "employee number";
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 FROM employees 
 WHERE hire_date LIKE '%1986';
 
--- 3. List the manager of each department with the following information: department number, 
--- department name, the manager's employee number, last name, first name.
-SELECT dm.dept_no, d.dept_name , dm.emp_no, e.last_name, e.first_name
-FROM employees e
-JOIN dept_manager dm
-ON dm.emp_no = e.emp_no
-LEFT JOIN departments d
-ON dm.dept_no = d.dept_no;
+-- 2. Alternative solution using date.
+SELECT first_name, last_name, hire_date
+FROM employees 
+WHERE to_date(hire_date, 'MM DD YYYY') BETWEEN '1986-01-01' AND '1986-12-31'
+ORDER BY to_date(hire_date, 'MM DD YYYY');
 
--- 4. List the department of each employee with the following information: employee number, 
--- last name, first name, and department name.
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT d.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+FROM departments d
+LEFT JOIN dept_manager dm
+ON dm.dept_no = d.dept_no
+LEFT JOIN employees e
+ON dm.emp_no = e.emp_no;
+
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees e
 LEFT JOIN dept_emp de
@@ -32,8 +37,7 @@ SELECT first_name, last_name, sex
 FROM employees 
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
--- 6. List all employees in the Sales department, including their employee number, last name, first name,
--- and department name.
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees e
 LEFT JOIN dept_emp de
@@ -42,18 +46,17 @@ LEFT JOIN departments d
 ON de.dept_no = d.dept_no
 WHERE d.dept_name = 'Sales';
 
--- 7. List all employees in the Sales and Development departments, including their employee number, 
--- last name, first name, and department name.
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees e
 LEFT JOIN dept_emp de
 ON de.emp_no = e.emp_no
 LEFT JOIN departments d
 ON de.dept_no = d.dept_no
-WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
+WHERE d.dept_name IN ('Sales', 'Development');
 
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
-SELECT last_name, COUNT(last_name) AS Count_of_last_name
+SELECT last_name, COUNT(last_name) AS count_of_last_name
 FROM employees
 GROUP BY last_name
-ORDER BY Count_of_last_name DESC;
+ORDER BY count_of_last_name DESC;
--- schema.sql
+++ schema.sql
@@ -1,58 +1,68 @@
 -- Drop tables if exist
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS titles;
-DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS departments CASCADE;
+DROP TABLE IF EXISTS titles CASCADE;
+DROP TABLE IF EXISTS employees CASCADE;
 DROP TABLE IF EXISTS salaries;
 DROP TABLE IF EXISTS dept_manager;
 DROP TABLE IF EXISTS dept_emp;
 
 -- Create new tables to import data
 CREATE TABLE departments (
-	dept_no VARCHAR PRIMARY KEY,
-	dept_name VARCHAR
+	dept_no VARCHAR NOT NULL PRIMARY KEY,
+	dept_name VARCHAR NOT NULL
 );
 
 CREATE TABLE titles (
-	title_id VARCHAR PRIMARY KEY,
-	title VARCHAR
+	title_id VARCHAR NOT NULL PRIMARY KEY,
+	title VARCHAR NOT NULL
 );
 
--- in this table dates are impoted as strings, beacuse american date formatting throws an error during import
+-- In this table dates are imported as strings, beacuse american date formatting in the csv file throws an error 
+-- during import when trying to import as date. They can be changed back to dates when querying by running 
+-- "to_date(birth_date, 'MM DD YYYY')".
+-- Alternatively this issue can be solved by manually changing the date format in excel prior to import. 
+-- Another option is to change the database datestyle with 'ALTER SYSTEM SET datestyle to "ISO, MDY"', however 
+-- according to stackoverflow this can create further issues with the database: "If at all possible, don't use 
+-- DATESTYLE. It'll affect all code in your session, including things like stored procedures that might not be 
+-- expecting it and might not have set an explicit overriding DATESTYLE in their definitions" by: 
+-- (https://stackoverflow.com/questions/13244460/how-to-change-datestyle-in-postgresql).
 CREATE TABLE employees (
-	emp_no INT PRIMARY KEY,	
-	emp_title_id VARCHAR,
+	emp_no INT NOT NULL PRIMARY KEY,	
+	emp_title_id VARCHAR NOT NULL,
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
-	birth_date VARCHAR,	
-	first_name VARCHAR,
-	last_name VARCHAR,
-	sex VARCHAR,	
-	hire_date VARCHAR
+	birth_date VARCHAR NOT NULL,	
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT NULL,	
+	hire_date VARCHAR NOT NULL
 );
 
 CREATE TABLE salaries (
-	emp_no INT PRIMARY KEY,
+	emp_no INT PRIMARY KEY NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	salary INT
+	salary INT NOT NULL
 );
 
 CREATE TABLE dept_manager (
-	dept_no VARCHAR,
+	dept_no VARCHAR NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	emp_no INT,
+	emp_no INT NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	PRIMARY KEY (dept_no, emp_no)
 );
 
 CREATE TABLE dept_emp (
-	emp_no INT,
+	emp_no INT NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	dept_no VARCHAR,
+	dept_no VARCHAR NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 
 -- Import data and view the tables
+SELECT * FROM departments LIMIT 10;
+SELECT * FROM titles LIMIT 10;
+SELECT * FROM employees LIMIT 10;
+SELECT * FROM salaries LIMIT 10;
+SELECT * FROM dept_manager LIMIT 10;
 SELECT * FROM dept_emp LIMIT 10;
-
-
-SELECT to_date(birth_date, 'MM DD YYYY') AS Birth_Date FROM employees LIMIT 10;
